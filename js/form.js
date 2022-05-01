f5658fe82910adbf084379674d42a6794e9aa54d
unknown <gabeclementej@gmail.com>
2022-05-01 19:52:29 -0300

    feat format functions form

---
+++ js/form.js
@@ -0,0 +1,47 @@
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+
+botaoAdicionar.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  var form = document.querySelector("#form-adiciona");
+
+  var paciente = getFormPacient(form);
+
+  var pacienteTr = montaTr(paciente);
+
+  var tabela = document.querySelector("#tabela-pacientes");
+  tabela.appendChild(pacienteTr);
+
+  form.reset()
+});
+
+function getFormPacient(form) {
+  var paciente = {
+    nome: form.nome.value,
+    peso: form.peso.value,
+    altura: form.altura.value,
+    gordura: form.gordura.value,
+    imc: calculaImc(form.peso.value, form.altura.value),
+  };
+  return paciente;
+}
+
+function montaTr(paciente) {
+  var pacienteTr = document.createElement("tr");
+  pacienteTr.classList.add("paciente");
+
+  pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+  pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
+  pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
+  pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
+  pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
+
+  return pacienteTr;
+}
+
+function montaTd(dado, classe) {
+  var td = document.createElement("td");
+  td.textContent = dado;
+  td.classList.add(classe);
+  return td;
+}
--- js/index.js
+++ js/index.js
@@ -1,50 +1,50 @@
-var titulo = document.querySelector(".header")
+var titulo = document.querySelector(".header");
 
 var pacientes = document.querySelectorAll(".paciente");
 
-for(var i = 0; i < pacientes.length ; i++){
-    var paciente = pacientes[i];
-    var tdPeso = paciente.querySelector(".info-peso");
-    var tdAltura = paciente.querySelector(".info-altura");
+for (var i = 0; i < pacientes.length; i++) {
+  var paciente = pacientes[i];
+  var tdPeso = paciente.querySelector(".info-peso");
+  var tdAltura = paciente.querySelector(".info-altura");
 
-    var tdImc = paciente.querySelector(".info-imc");
+  var tdImc = paciente.querySelector(".info-imc");
 
-    var peso = tdPeso.textContent;
-    var altura = tdAltura.textContent;
+  var peso = tdPeso.textContent;
+  var altura = tdAltura.textContent;
 
-    var alturaEhValida = true;
-    var pesoEhValido = true;
+  var alturaEhValida = true;
+  var pesoEhValido = true;
 
-    if(peso <= 0 || peso >= 1000){
-        console.log("Peso inválido");
-        tdImc.textContent = "Peso inválido!";
-        pesoEhValido = false;
-        paciente.classList.add("paciente-invalido");
-    }
+  if (peso <= 0 || peso >= 1000) {
+    console.log("Peso inválido");
+    tdImc.textContent = "Peso inválido!";
+    pesoEhValido = false;
+    paciente.classList.add("paciente-invalido");
+  }
 
-    if(altura <= 0 || altura >= 3.0){
-        console.log("Altura inválida");
-        tdImc.textContent = "Altura inválida!";
-        alturaEhValida = false;
-        paciente.classList.add("paciente-invalido");
-    }
+  if (altura <= 0 || altura >= 3.0) {
+    console.log("Altura inválida");
+    tdImc.textContent = "Altura inválida!";
+    alturaEhValida = false;
+    paciente.classList.add("paciente-invalido");
+  }
 
-    if(pesoEhValido && alturaEhValida){
-        var imc = peso / ( altura * altura);    
-        tdImc.textContent = imc.toFixed(2);
-    }    
+  if (pesoEhValido && alturaEhValida) {
+    var imc = calculaImc(peso,altura);
+    tdImc.textContent = imc;
+  }
+
+  function calculaImc(peso,altura){
+    var imc = 0;
+    imc = peso / (altura * altura)
+    return imc.toFixed(2);
 }
 
-titulo.addEventListener("click", mostraMensagem)
-
-function mostraMensagem(){
-    console.log("Ola eu fui clicado")
 }
 
-var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click", function(event){
-    event.preventDeafult();
+titulo.addEventListener("click", mostraMensagem);
 
-    var form = document.querySelector("#form-adiciona")
-    
-})
+function mostraMensagem() {
+  console.log("Ola eu fui clicado");
+}
+
