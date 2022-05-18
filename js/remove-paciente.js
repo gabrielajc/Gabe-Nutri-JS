819b1925c0d7923037eb92bac17b1de3776b5d01
unknown <gabeclementej@gmail.com>
2022-05-18 18:20:11 -0300

    feat footer

--- js/remove-paciente.js
+++ js/remove-paciente.js
@@ -1,9 +1,5 @@
-var tabela = document.querySelector("#tabela-pacientes");
+var tabela = document.querySelector("table");
 
-tabela.addEventListener("dblclick", function(event) {
-    event.target.parentNode.classList.add("fadeOut");
-    
-    setTimeout(function() {
-        event.target.parentNode.remove();
-    }, 500);
+tabela.addEventListener("dblclick",function(event){
+    event.target.parentNode.remove();
 });
